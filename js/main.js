53fc5a6a3802421439f3a0cf05f1976a98269e5b
Samuele Catania <samuele.catania@hotmail.com>
2025-11-19 19:04:21 +0100

    sidebar
    Dynamically create sidebar

--- js/main.js
+++ js/main.js
@@ -1,34 +1,49 @@
-const nlEls = document.querySelectorAll('.nl');
 const versEl = document.querySelectorAll('version');
 
 document.querySelector('build').innerHTML = '20-11-2025';
 
-nlEls.forEach(nl => {
-    nl.addEventListener('click', (e) => {
-        // prevent parent .nl handlers from also toggling when nested .nl clicked
-        e.stopPropagation();
-
-        const arrow = nl.querySelector('.arrow');
-        const subList = nl.querySelector('ul');
-
-        if (!arrow) return;
-
-        const isCollapsed = arrow.classList.contains('collapsed');
-
-        if (isCollapsed) {
-            arrow.classList.remove('collapsed');
-            arrow.classList.add('expanded');
-            // clear inline style so CSS controls layout (removes display: none)
-            if (subList) subList.style.display = '';
-        } else {
-            arrow.classList.remove('expanded');
-            arrow.classList.add('collapsed');
-            // hide sublist using inline style
-            if (subList) subList.style.display = 'none';
-        }
-    });
-});
-
 versEl.forEach(ver => {
     ver.innerHTML = '6000.0.62f1';
 })
+
+document.addEventListener("DOMContentLoaded", () => {
+    const sidebarEl = document.getElementById("sidebar");
+    
+    fetch("../Templates/Sidebar.sidebar")
+    .then(res => res.text())
+    .then(html => {
+        sidebarEl.innerHTML = html;
+
+        AddSidebarListener();
+    })
+});
+
+function AddSidebarListener() {
+    const nlEls = document.querySelectorAll('.nl');
+
+    nlEls.forEach(nl => {
+        nl.addEventListener('click', (e) => {
+            // prevent parent .nl handlers from also toggling when nested .nl clicked
+            e.stopPropagation();
+
+            const arrow = nl.querySelector('.arrow');
+            const subList = nl.querySelector('ul');
+
+            if (!arrow) return;
+
+            const isCollapsed = arrow.classList.contains('collapsed');
+
+            if (isCollapsed) {
+                arrow.classList.remove('collapsed');
+                arrow.classList.add('expanded');
+                // clear inline style so CSS controls layout (removes display: none)
+                if (subList) subList.style.display = '';
+            } else {
+                arrow.classList.remove('expanded');
+                arrow.classList.add('collapsed');
+                // hide sublist using inline style
+                if (subList) subList.style.display = 'none';
+            }
+        });
+    });
+};
